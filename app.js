27cedaf3633250016d0efd891271dbec2e139485
Tanush <tanushr20@gmail.com>
2025-07-06 14:49:55 +0530

    General fix

--- app.js
+++ app.js
@@ -86,6 +86,10 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.currUser = req.user;
+  next();
+});
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
